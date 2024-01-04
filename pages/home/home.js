7b9db23cee0b4d4322177062b4a188a2d211bf2b
Lin <1439713475@qq.com>
2024-01-04 17:16:03 +0800

    feat: add banner

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,7 +4,44 @@ Page({
       delta: 1 
     });
   },
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500
+  },
   onClickRight() {
     wx.showToast({ title: '点击按钮', icon: 'none' });
   },
+  changeIndicatorDots() {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+
+  changeAutoplay() {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
+    }
+  },
+
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
 });
